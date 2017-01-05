5934775b40d7d841333b5ceb235d69034385da2f
Pete Alexandrou <pete@ozmartians.com>
2017-01-05 18:04:35 +0800

    cleanup + setuptools setting up Linux icons and desktop file

--- setup.py
+++ setup.py
@@ -19,7 +19,21 @@ def get_version(filename='__init__.py'):
 
 def get_description(filename='README.md'):
     with open(path.join(here, filename), 'r') as f:
-        return f.read()
+        file = list(f)
+    desc = ''
+    for item in file[8: len(file)]:
+        desc += item
+    return desc
+
+
+def get_data_files():
+    if sys.platform.startswith('linux'):
+        return [
+            ('/usr/share/pixmaps', ['data/pixmaps/vidcutter.png']),
+            ('/usr/share/applications', ['data/desktop/vidcutter.desktop'])
+        ]
+    else:
+        return []
 
 
 setup(
@@ -28,19 +42,19 @@ setup(
     author='Pete Alexandrou',
     author_email='pete@ozmartians.com',
     description='''tv-release.net link scraper integrated with real-debrid to unrestrict links + supporting
-                   Aria2 RPC Daemon (windows/linux), kget (linux), pyLoad (windows/linux), Internet Download Manager (windows).
-                   A built-in downloader is the default setting.''',
+                   Aria2 RPC Daemon (windows/linux), kget (linux), pyLoad (windows/linux), Internet Download Manager
+                   (windows). A built-in downloader is enabled by default.''',
     long_description=get_description(),
     url='http://tvlinker.ozmartians.com',
     license='GPLv3+',
     packages=['tvlinker'],
     package_dir={'tvlinker': '.'},
-    setup_requires=['setuptools >= 28.1.0'],
-    install_requires=['PyQt5 >= 5.5', 'beautifulsoup4 >= 4.5.1', 'QtAwesome >= 0.3.3'],
-    extras_require={':sys_platform!="win32"': ['lxml >= 3.6.4']},
+    setup_requires=['setuptools >= 26.1.1'],
+    install_requires=['beautifulsoup4', 'QtAwesome'],
+    extras_require={':sys_platform!="win32"': ['lxml']},
     package_data={'tvlinker': ['tvlinker.ini', 'assets/images/tvlinker.ico', 'assets/images/tvlinker.png']},
     entry_points={'gui_scripts': ['tvlinker = tvlinker.tvlinker:main']},
-    keywords='tvlinker scraping tv-release filesharing',
+    keywords='tvlinker scraping tv-release filesharing internet',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: X11 Applications :: Qt',
