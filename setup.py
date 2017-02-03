ec1f8f732f1ceb1d5fbdd72325109d9327301b02
Pete Alexandrou <pete@ozmartians.com>
2017-02-04 06:28:33 +0800

    .

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_description(filename='README.md'):
 
 def get_install_requires():
     if packager == 'pypi':
-        return ['PyQt5 >= 5.5', 'beautifulsoup4', 'lxml']
+        return ['PyQt5', 'beautifulsoup4', 'lxml']
     else:
         return []
 
