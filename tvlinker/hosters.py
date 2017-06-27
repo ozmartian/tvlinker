9e8a7990652cce07a3c37208bf42c5b5057fb344
Pete Alexandrou <pete@ozmartians.com>
2017-06-28 05:27:08 +0800

    sizing fixes

--- tvlinker/hosters.py
+++ tvlinker/hosters.py
@@ -30,11 +30,11 @@ class HosterLinks(QDialog):
         self.setLayout(self.layout)
         self.setWindowTitle('Hoster Links')
         self.setWindowModality(Qt.ApplicationModal)
+        self.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
 
     def show_hosters(self, links: list) -> None:
         self.links = links
         self.loading_progress.cancel()
-        self.setMinimumWidth(790)
         hosterswidget_layout = QVBoxLayout()
         for tag in self.links:
             title_label = QLabel(HosterLinks.bs_tag_to_string(tag.find_previous('p')), self)
@@ -43,6 +43,8 @@ class HosterLinks(QDialog):
             title_label.setStyleSheet('QLabel { margin: 0; color: #444; font-size: 14px; padding: 8px; ' +
                                       'border: 1px solid #C0C0C0; background-color: #FEFEFE; }')
             title_layout = QHBoxLayout()
+            title_layout.setContentsMargins(0, 0, 0, 0)
+            title_layout.setSpacing(6)
             title_layout.addStretch(1)
             bs = BeautifulSoup(HosterLinks.bs_tag_to_string(tag), 'lxml', parse_only=SoupStrainer('a'))
             for anchor in bs:
@@ -54,8 +56,8 @@ class HosterLinks(QDialog):
                 menu.addAction('  OPEN LINK', lambda: self.open_link(link), 0)
                 menu.addAction(' DOWNLOAD', lambda: self.download_link(link), 0)
                 shadow = QGraphicsDropShadowEffect()
-                shadow.setColor(Qt.darkGray)
-                shadow.setBlurRadius(5)
+                shadow.setColor(Qt.gray)
+                shadow.setBlurRadius(10)
                 shadow.setOffset(2, 2)
                 hoster_btn = QPushButton(self)
                 hoster_btn.setStyle(QStyleFactory.create('Fusion'))
@@ -74,9 +76,14 @@ class HosterLinks(QDialog):
                         border-radius: 0;
                         border: 1px solid #B9B9B9;
                     }
-                    QPushButton:pressed, QPushButton:hover {
+                    QPushButton:hover {
                         background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
                                                           stop: 0 #B9B9B9, stop: 1 #DADBDE);
+                    }
+                    QPushButton:pressed {
+                        border: 1px inset #B9B9B9;
+                        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                                          stop: 0 #DADBDE, stop: 1 #B9B9B9);
                     }
                     QPushButton::menu-indicator { left: -2000px; }
                 ''')
@@ -99,20 +106,25 @@ class HosterLinks(QDialog):
             hosterswidget_layout.addLayout(hoster_layout)
             hosterswidget_layout.addSpacing(15)
 
-        hosters_widget = QWidget(self)
-        hosters_widget.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.MinimumExpanding)
-        hosters_widget.setMinimumWidth(770)
         stretch_layout = QHBoxLayout()
         stretch_layout.addStretch(1)
         stretch_layout.addLayout(hosterswidget_layout)
         stretch_layout.addStretch(1)
+        hosters_widget = QWidget(self)
         hosters_widget.setLayout(stretch_layout)
         scrollarea = QScrollArea(self)
         scrollarea.setStyleSheet('QScrollArea { border:none; }')
         scrollarea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         scrollarea.setWidget(hosters_widget)
+        scrollarea.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        scrollarea.setMinimumWidth(hosters_widget.geometry().width() + 20)
         self.layout.addWidget(scrollarea)
-        h = hosters_widget.height() + 10
+        w = hosters_widget.geometry().width() + 15
+        if w <= 810:
+            self.setMinimumWidth(w)
+        else:
+            self.setMaximumWidth(810)
+        h = hosters_widget.geometry().height() + 10
         if h <= 750:
             self.setMinimumHeight(h)
         else:
