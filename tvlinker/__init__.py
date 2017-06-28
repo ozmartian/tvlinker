adc1eaa9e509aadfe9b11e5c1b5efd3898d6642a
Pete Alexandrou <pete@ozmartians.com>
2017-06-28 20:41:46 +0800

    .

--- tvlinker/__init__.py
+++ tvlinker/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import sys
-
-from PyQt5.QtCore import QFileInfo
 
 
 __version__ = '4.0.0'
 __packager__ = 'pypi' # (pypi, arch, deb, rpm)
 
-sys.path.insert(0, QFileInfo(__file__).absolutePath())
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
