1a81ebf311e7530eedd0fac1d4095244918ec27f
Pete Alexandrou <pete@ozmartians.com>
2018-05-29 13:41:47 +0800

    version bump

--- tvlinker/__init__.py
+++ tvlinker/__init__.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 
-__version__ = '4.0.5'
+__version__ = '4.2.0'
 __packager__ = 'pypi' # (pypi, arch, deb, rpm)
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
